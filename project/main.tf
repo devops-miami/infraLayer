82a38b15ba26cb4263c4e670b891c6d6aeab3aa0
iamleet <rickalv.it@gmail.com>
2020-12-14 23:18:58 -0500

    Adding the first call to the first module for the demo project

--- project/main.tf
+++ project/main.tf
@@ -1,1 +1,7 @@
-# changes
+# Demo for using Terraform 
+# devops.miami by Rick Alvarez
+##
+module gke-cluster {
+    source          = "./modules/google_container_cluster"
+    clusters        =  var.clusters
+}
